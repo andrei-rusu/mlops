68b5fbc4f0f89266902f2bec09f930e16b0f4f16
Andrei Rusu <rusu.c.andrei@gmail.com>
2024-04-10 14:09:41 +0300

    Add code to solve challenges 0, 1, 2.

---
+++ production/data_inspect.py
@@ -0,0 +1,14 @@
+import mltable
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+
+ml_client = MLClient.from_config(credential=DefaultAzureCredential())
+data_asset = ml_client.data.get("diabetes-last-training-data", version="1")
+
+path = {
+  'folder': data_asset.path
+}
+
+tbl = mltable.from_delimited_files(paths=[path])
+df = tbl.to_pandas_dataframe()
+print(df.head(5))
---
+++ src/job_submit.py
@@ -0,0 +1,24 @@
+from azure.ai.ml import MLClient, command, Input
+from azure.identity import DefaultAzureCredential
+
+ml_client = MLClient.from_config(credential=DefaultAzureCredential())
+
+# Define the command job
+command_job = command(
+    code="./src/model",
+    command="python train.py --training_data ${{inputs.training_data}} --reg_rate ${{inputs.reg_rate}}",
+    inputs={
+        "training_data": Input(type="uri_folder", path="azureml:diabetes-last-training-data:1"),
+        "reg_rate": 0.01
+    },
+    environment="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu@latest",
+    compute="aml-cluster",
+    experiment_name="diabetes-train",
+    description="Job to train a diabetes model."
+)
+
+# Submit the command job
+returned_job = ml_client.jobs.create_or_update(command_job)
+
+# Print the job details
+print(returned_job)
