75d1bdb1bfd6034c42d798368184bedc181a27d6
Andrei Rusu <rusu.c.andrei@gmail.com>
2024-04-10 16:30:17 +0300

    Fix linting problems.

--- src/model/train.py
+++ src/model/train.py
@@ -36,8 +36,8 @@ def get_csvs_df(path):
 
 def split_data(df):
     print("Splitting data...")
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness',
-    'SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 
+               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
@@ -64,6 +64,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
